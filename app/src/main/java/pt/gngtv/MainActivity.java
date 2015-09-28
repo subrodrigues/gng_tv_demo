9c26f9a73d7d5fb62d76040e46ab1277460e5c9c
Eduardo Pereira <epereira@thing-pink.pt>
2015-09-28 10:36:19 +0100

    First commit.

---
+++ app/src/main/java/pt/gngtv/MainActivity.java
@@ -0,0 +1,16 @@
+package pt.gngtv;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by joseaguiar on 25/09/15.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
